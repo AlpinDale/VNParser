c92efb1b46250ec73c6e548fbd60d1fa439bf8c9
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-01-25 00:03:44 +0430

    Update baldr-sky-cleanup.py

--- NeXAs/baldr-sky-cleanup.py
+++ NeXAs/baldr-sky-cleanup.py
@@ -9,8 +9,9 @@ new_lines = []
 
 for line in lines:
     line = re.sub(r'\\t\S{4}', '', line)
-    line = re.sub(r'\\s\S{4}', '', line)
+    line = re.sub(r'\\t\S{4}', '', line)
     line = re.sub(r'\\f\S{2}', '', line)
+    line = re.sub(r'\\v\S{9}', '', line)
     line = re.sub(r'\\r\\', '', line)
     line = re.sub(r'\\h(KOU|RAIN|CHINATSU|NANOHA|MAKOTO|SORA|AKI|SEIRA|KUU|ANAN|GILBERT|EIJI|ISAO|NAOKI|NOI|SHIZEL|MASA)\S{6}', '', line)
     new_lines.append(line)
@@ -27,7 +28,7 @@ lines = lines.replace("\h", "")
 lines = lines.replace("\r", "")
 lines = lines.replace("\t", "")
 lines = lines.replace("\s", "")
+lines = lines.replace("\e", "")
 
 with open(file_name, 'w') as f:
     f.write(lines)
-
