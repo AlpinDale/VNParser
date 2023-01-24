8feef9d184a6d60715f15bd5cd72429f98998951
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-01-24 23:47:02 +0430

    Add files via upload

---
+++ NeXAs/baldr-sky-cleanup.py
@@ -0,0 +1,30 @@
+import re
+
+file_name = input("Enter the file name (with extension): ")
+
+with open(file_name, 'r') as f:
+    lines = f.readlines()
+
+new_lines = []
+
+for line in lines:
+    line = re.sub(r'\\t\S{4}', '', line)
+    line = re.sub(r'\\r\\', '', line)
+    line = re.sub(r'\\h(KOU|RAIN|CHINATSU|NANOHA|MAKOTO|SORA|AKI|SEIRA|KUU|ANAN|GILBERT|EIJI|ISAO|NAOKI|NOI|SHIZEL|MASA)\S{6}', '', line)
+    new_lines.append(line)
+
+with open(file_name, 'w') as f:
+    for line in new_lines:
+        f.write(line)
+
+# remove remaining instances of \t, \h, and \r
+with open(file_name, 'r') as f:
+    lines = f.read()
+
+lines = lines.replace("\h", "")
+lines = lines.replace("\r", "")
+lines = lines.replace("\t", "")
+
+with open(file_name, 'w') as f:
+    f.write(lines)
+
