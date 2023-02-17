d0016bdaa5207678ce46351fb7c16819fe3edbb2
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-02-17 15:31:36 +0430

    Script updated to handle different encodings

--- script-combiner.py
+++ script-combiner.py
@@ -3,12 +3,13 @@ import os
 
 directory = './'
 extension = input("Enter the extension of the files you want to combine (e.g. '.txt' or '.rpy'): ")
+encoding = input("Enter the encoding of the files (e.g. 'shift_jis'): ")
 files = sorted(glob.glob(directory + '*' + extension))
-combined_file = open(directory + 'combined' + extension, 'w')
+combined_file = open(directory + 'combined' + extension, 'w', encoding='utf-8')
 for filename in files:
     separator = '\n' + '\n' * 5 + '=' * 10 + os.path.basename(filename) + '=' * 10 + '\n'
     combined_file.write(separator)
-    with open(filename) as f:
+    with open(filename, encoding=encoding) as f:
         for line in f:
             combined_file.write(line)
     combined_file.write('\n' * 5)
